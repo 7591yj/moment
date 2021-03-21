2bd1df0f3d1d5ae14fdb6af5d19235497a7a605c
Yeongjin Kim <7591blitzloft@gmail.com>
2021-03-21 14:40:11 +0900

    Update bg.js
    Gets image from web, no local

--- bg.js
+++ bg.js
@@ -5,23 +5,44 @@ const IMG_NUM = 6;
 
 function paintImage(imgNumber) {
   const image = new Image();
-  image.src = `images/${imgNumber + 1}.jpg`;
-  image.classList.add("bgImage");
-  body.prepend(image);
   if (imgNumber + 1 === 1) {
     from.innerText = "\uD83D\uDD88 Emerald Bay, United States";
+    image.src =
+      "https://cdn.discordapp.com/attachments/749147585792573522/823067546626031656/1.jpg";
+    image.classList.add("bgImage");
+    body.prepend(image);
   } else if (imgNumber + 1 === 2) {
     from.innerText =
       "\uD83D\uDD88 El Cap, Yosemite National Park, United States";
+    image.src =
+      "https://cdn.discordapp.com/attachments/749147585792573522/823067690615963648/2.jpg";
+    image.classList.add("bgImage");
+    body.prepend(image);
   } else if (imgNumber + 1 === 3) {
     from.innerText = "\uD83D\uDD88 Rieserferner-Ahrn Nature Park, Italy";
+    image.src =
+      "https://cdn.discordapp.com/attachments/749147585792573522/823067706709770270/3.jpg";
+    image.classList.add("bgImage");
+    body.prepend(image);
   } else if (imgNumber + 1 === 4) {
     from.innerText =
       "\uD83D\uDD88 Great Smoky Mountains National Park, United States";
+    image.src =
+      "https://cdn.discordapp.com/attachments/749147585792573522/823067711600459786/4.jpg";
+    image.classList.add("bgImage");
+    body.prepend(image);
   } else if (imgNumber + 1 === 5) {
     from.innerText = "\uD83D\uDD88 Whangarei Falls, Whangarei, New Zealand";
+    image.src =
+      "https://cdn.discordapp.com/attachments/749147585792573522/823067727882878986/5.jpg";
+    image.classList.add("bgImage");
+    body.prepend(image);
   } else if (imgNumber + 1 === 6) {
     from.innerText = "\uD83D\uDD88 Bad Pyrmont, Deutschland";
+    image.src =
+      "https://cdn.discordapp.com/attachments/749147585792573522/823067743543885834/6.jpg";
+    image.classList.add("bgImage");
+    body.prepend(image);
   }
 }
 
